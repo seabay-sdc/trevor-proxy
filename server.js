a259496fd7eca3cb944e82d6e674913d95d35a20
Trevor Cannon <cannon.trevor@gmail.com>
2019-07-23 15:31:48 -0500

    found missing files and added them

--- server.js
+++ server.js
@@ -3,7 +3,7 @@ const express = require('express');
 const cors = require('cors');
 const compression = require('compression');
 const app = express();
-const port = process.env.SERVER_PORT;
+const port = 3000;
 
 app.use(cors());
 app.use(compression());
@@ -11,4 +11,4 @@ app.use('/', express.static('public'));
 app.use('/scripts', express.static('scripts'));
 app.use(express.json());
 
-app.listen(port, () => console.log(`Listening on ${port}`));
+app.listen(port, () => console.log(`Serving it up on ${port}`));
